01dbb08b3f78ae8fa7270cc6b26164dd81932d74
jersu39 <jersu039@gmail.com>
2026-02-27 14:15:06 -0800

    update

--- sql/09.sql
+++ sql/09.sql
@@ -3,4 +3,4 @@
  * Use tables staff and payment.
  */
 
-select first_name, last_name, sum(amount) as sum from staff join payment on staff.staff_id = payment.staff_id where payment.payment_date >= '1/1/2020' and payment.payment_date < '2/1/2020' group by staff.staff_id order by last_name asc;
+select first_name, last_name, sum(amount) as sum from staff join payment on staff.staff_id = payment.staff_id where payment.payment_date >= '1/1/2020' and payment.payment_date < '2/1/2020' group by staff.staff_id order by last_name desc;
